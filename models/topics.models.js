10bfb7d74a0b0f362bc95e2e7686737d546791ab
Alex <alexdtaylor93@gmail.com>
2024-06-12 15:06:20 +0100

    fix bugs in topics and user models

--- models/topics.models.js
+++ models/topics.models.js
@@ -1,13 +1,8 @@
 const db = require("../db/connection");
 
 const fetchTopics = () => {
-  return db
-    .query(`SELECT * FROM topics;`)
-    .then((topics) => {
-      return topics.rows;
-    })
-    .catch((err) => {
-      next(err);
-    });
+  return db.query(`SELECT * FROM topics;`).then((topics) => {
+    return topics.rows;
+  });
 };
 module.exports = { fetchTopics };
